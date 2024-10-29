90e806942c82ee9faba7cce2add964b69b879a9b
MoAdel <madel7586@gmail.com>
2024-10-29 23:09:10 +0300

    Add assets folder and assets.dart file

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for images3dHouse
+  /// assets/images/3d-house.png
+  static const String images3dHouse = "assets/images/3d-house.png";
+
+  /// Assets for imagesImage
+  /// assets/images/image.png
+  static const String imagesImage = "assets/images/image.png";
+
+  /// Assets for imagesNoroot
+  /// assets/images/noroot.png
+  static const String imagesNoroot = "assets/images/noroot.png";
+
+  /// Assets for imagesPngwingCom
+  /// assets/images/pngwing.com.png
+  static const String imagesPngwingCom = "assets/images/pngwing.com.png";
+
+  /// Assets for imagesRb26088
+  /// assets/images/rb_26088.png
+  static const String imagesRb26088 = "assets/images/rb_26088.png";
+
+  /// Assets for imagesWatch
+  /// assets/images/watch.png
+  static const String imagesWatch = "assets/images/watch.png";
+}
+

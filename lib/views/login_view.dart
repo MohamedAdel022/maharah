4fb25b456fef22a33d318bdb9ca493a321b52d16
MoAdel <madel7586@gmail.com>
2024-10-30 20:23:27 +0300

    Add LoginView and update main app entry point

---
+++ lib/views/login_view.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maharah/constants.dart';
+import 'package:maharah/core/utils/app_images.dart';
+import 'package:maharah/views/widgets/language_button.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          backgroundColor: const Color(0xffEFEFEF),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const LanguageButton(),
+                  Center(
+                    child: SvgPicture.asset(
+                      Assets.imagesHomeTopBannerLogo,
+                      height: 200,
+                      width: 200,
+                      colorFilter:
+                          const ColorFilter.mode(textColor, BlendMode.srcIn),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Center(
+                    child: Text(
+                      'Welcome Back',
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          color: Colors.white,
+                        ),
+                        child: const Center(
+                            child: Text(
+                          '+965',
+                          style: TextStyle(color: textColor, fontSize: 16),
+                        )),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 50,
+                          padding: const EdgeInsets.all(10),
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16)),
+                            color: Colors.white,
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Enter your phone number',
+                              hintStyle: TextStyle(color: greyColor),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                      color: textColor,
+                    ),
+                    child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign in',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        )),
+                  ),
+                  const SizedBox(height: 8),
+                  const Center(
+                    child: Text('Start as a guest',
+                        style: TextStyle(
+                            color: textColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Don\'t have an account?',
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                      TextButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.all(0),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'Sign up',
+                            style: TextStyle(
+                                color: textColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          )),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
